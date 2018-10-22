9d806092939126408187377314a77905b076249a
Gabiginez <gabi_ginez@hotmail.com>
2018-10-22 07:21:07 -0300

    Radar

---
+++ Inter/Assets/Scripts/RadarCamera.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class RadarCamera : MonoBehaviour {
+
+	public GameObject Player;
+
+	public GameObject[] TrackedObjects;
+	IList<GameObject> radarObjects;
+	public GameObject radarPrefab;
+	IList<GameObject> borderObjects;
+	public float switchDistance;
+	public Transform helpTransform;
+
+	// Use this for initialization
+	void Start () {
+
+		CreateRadarObjects ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Player.transform.position;
+		transform.rotation = Quaternion.Euler(0, Player.transform.rotation.eulerAngles.y, 0);
+
+		for (int i = 0; i < radarObjects.Count; i++) 
+		{
+			if (Vector3.Distance (radarObjects [i].transform.position, transform.position) > switchDistance) 
+			{
+				helpTransform.LookAt (radarObjects[i].transform);
+				borderObjects [i].transform.position = transform.position + switchDistance * helpTransform.forward;
+				borderObjects [i].layer = LayerMask.NameToLayer ("Radar");
+				radarObjects [i].layer = LayerMask.NameToLayer ("Invisible");
+			} 
+			else 
+			{
+				borderObjects [i].layer = LayerMask.NameToLayer ("Invisible");
+				radarObjects [i].layer = LayerMask.NameToLayer ("Radar");
+			}
+		}
+		
+	}
+
+	void CreateRadarObjects()
+	{
+
+		radarObjects = new List<GameObject> ();
+		borderObjects = new List<GameObject> ();
+		foreach(GameObject o in TrackedObjects)
+		{
+			GameObject k = Instantiate (radarPrefab, o.transform.position, Quaternion.identity) as GameObject;
+			radarObjects.Add (k);
+			GameObject j = Instantiate (radarPrefab, o.transform.position, Quaternion.identity) as GameObject;
+			radarObjects.Add (j);
+		}
+		
+	}
+}
